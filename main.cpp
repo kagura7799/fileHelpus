1c149fef5b313773e77039af4e01a4f514704cf2
kagura <mkotelnikov97@gmail.com>
2024-03-09 21:41:42 +0300

    was added user interaction

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 ﻿#include "../fileHelpus/include/fileReader.h"
 #include <iostream>
 
+int countWordsInFile(std::string filename);
+
 int main() {
-    std::string filename = "example.txt"; 
+    std::string filename;
+    std::cout << "Выберите файл для работы: ";
+    std::cin >> filename;
+    std::cout << std::endl;
+
+    char action;
+    std::cout << "Что вы хотите сделать?" << std::endl;
+    std::cout << "0 - Найти кол-во слов в файле" << std::endl;
+    std::cin >> action;
+
+    switch (action)
+    {
+    case '0': countWordsInFile(filename);
+        break;
+    default:
+        std::cerr << "Ошибка. Выберите существующее действие." << std::endl;
+        return -1;
+        break;
+    }
+    return 0;
+}
+
+int countWordsInFile(std::string filename)
+{
     FileReader reader(filename);
     int wordCount = reader.countWords();
 
@@ -12,6 +37,4 @@ int main() {
     }
 
     std::cout << "Number of words in file: " << wordCount << std::endl;
-
-    return 0;
 }
